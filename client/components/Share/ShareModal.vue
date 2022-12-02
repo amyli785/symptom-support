34adadf358e04fc6366fa82637498407c88fded4
irinaz <irinaz@mit.edu>
2022-12-02 16:27:47 -0500

    change text on share modal

--- client/components/Share/ShareModal.vue
+++ client/components/Share/ShareModal.vue
@@ -14,7 +14,7 @@
                 <a :href="shareLink">{{this.shareLink}}</a>
             </section>
             <section v-if="!shareCreated">
-                <p>Sharing Entries: </p>
+                <p>Sharing Entries:</p>
                 <div class="entries">
                     <EntryComponent
                         v-for="entry in entries"
