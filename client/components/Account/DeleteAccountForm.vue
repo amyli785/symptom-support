02a9d4bac76fa93e3d27449d13954393ca69da97
irinaz <irinaz@mit.edu>
2022-12-11 03:12:04 -0500

    add confirmation for deleting account

--- client/components/Account/DeleteAccountForm.vue
+++ client/components/Account/DeleteAccountForm.vue
@@ -1,26 +1,93 @@
-<!-- Form for deleting account (block style) -->
+<template>
+    <form>
+        <h3>Delete account</h3>
+        <p>Deleting your account is permanent and irreversible. Proceed only if you understand these consequences.</p>
+        <button @click="showDeleteAccountModal">Delete account</button>
+
+        <ConfirmDeleteModal class="modal"
+        itemName = "account"
+        :itemId = "$store.state.username"
+        :deleteFunction = "this.deleteAccount"
+        />
+    </form>
+</template>
 
 <script>
-import BlockForm from '@/components/common/BlockForm.vue';
+
+import ConfirmDeleteModal from "../common/ConfirmDeleteModal.vue";
 
 export default {
-  name: 'DeleteAccountForm',
-  mixins: [BlockForm],
-  data() {
-    return {
-      url: '/api/users',
-      method: 'DELETE',
-      setUsername: true,
-      title: 'Delete account',
-      fields: [],
-      content: 'Deleting your account is permanent and irreversible. Proceed only if you understand these consequences.',
-      callback: () => {
-        this.$router.push({name: 'Home'}); // Goes to Home page after deleting account
-        this.$store.commit('alert', {
-          message: 'Your account has been deleted!', status: 'success'
-        });
-      }
-    };
-  }
-};
+    name: 'DeleteAccountForm',
+    components: {
+        ConfirmDeleteModal
+    }, 
+    methods: {
+        showDeleteAccountModal(){
+            this.$bvModal.show(`confirm-delete-modal-account-${this.$store.state.username}`);
+        },
+        async deleteAccount() {
+            const options = {
+                method: "DELETE",
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'same-origin' // Sends express-session credentials with request
+            };
+            try {
+                const r = await fetch('/api/users', options);
+                if (!r.ok) {
+                    const res = await r.json();
+                    throw new Error(res.error);
+                }
+                const text = await r.text();
+                const res = text ? JSON.parse(text) : {user: null};
+                this.$store.commit('setUsername', res.user ? res.user.username : null);
+                this.$store.commit('setDisplayName', res.user ? res.user.displayName : null);
+
+                this.$router.push({name: 'Home'}); // Goes to Home page after deleting account
+                this.$store.commit('alert', {
+                message: 'Your account has been deleted!', status: 'success'
+                });
+
+            } catch (e) {
+                this.$set(this.alerts, e, 'error');
+                setTimeout(() => this.$delete(this.alerts, e), 3000);
+            }
+        }
+    }
+}
 </script>
+
+<style scoped>
+form {
+  background-color: #ffffff;
+  filter: drop-shadow(0 0 2px var(--dark-blue-drop-shadow));
+  color: black;
+  border-radius: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  position: relative;
+}
+button{
+  padding: 5px;
+  border-radius: 10px;
+}
+article > div {
+  display: flex;
+  flex-direction: column;
+}
+form > article p {
+  margin: 0;
+}
+form h3,
+form > * {
+  margin: 0.3em 0;
+}
+form h3 {
+  margin-top: 0;
+}
+.modal{
+    padding: 0 !important;
+}
+</style>
