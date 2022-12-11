55721c6a3e3e2bf698933dd4e5541077b910292c
Gustavo Martins <gustavoam00@gmail.com>
2022-12-11 13:51:40 -0500

    x button

---
+++ client/components/common/XButton.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="x-button-container icon-button" @click="$emit('click')">
+    <font-awesome-icon
+    class = "i"
+      icon="fa-solid fa-x"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XButton',
+}
+</script>
+
+<style scoped>
+
+.x-button-container {
+  display: flex;
+
+  flex-direction: column;
+  justify-self: center;
+  align-items: center;
+}
+.i{
+  font-size: 30px;
+}
+</style>
